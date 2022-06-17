9043a479be8b6c409f9dae50f4963903519ca6c6
Reuben Taveras <reubentaveras@Reubens-MBP.cfl.rr.com>
2022-06-17 13:59:21 -0400

    Huh

---
+++ my-blog-frontend/src/pages/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutPage = () => (
+	<>
+		<h1>About Me</h1>
+
+		<p>In ut sunt qui nostrud est consequat ut mollit ullamco aliquip dolor magna. Laborum magna ut eu incididunt nisi sit ullamco sint anim ex. Exercitation pariatur laborum non anim duis magna laboris velit officia ea adipisicing. Fugiat sint duis magna consectetur cupidatat ut voluptate anim et. Cillum laborum exercitation officia officia fugiat voluptate et laborum ut. Lorem ipsum dolor veniam culpa sint laboris reprehenderit cillum labore sunt sit ut in in ut aliquip excepteur id magna duis. Voluptate non ut minim reprehenderit aute et aliquip esse labore veniam laborum mollit.</p>
+
+		<p>In ut sunt qui nostrud est consequat ut mollit ullamco aliquip dolor magna. Laborum magna ut eu incididunt nisi sit ullamco sint anim ex. Exercitation pariatur laborum non anim duis magna laboris velit officia ea adipisicing. Fugiat sint duis magna consectetur cupidatat ut voluptate anim et. Cillum laborum exercitation officia officia fugiat voluptate et laborum ut. Lorem ipsum dolor veniam culpa sint laboris reprehenderit cillum labore sunt sit ut in in ut aliquip excepteur id magna duis. Voluptate non ut minim reprehenderit aute et aliquip esse labore veniam laborum mollit.</p>
+
+		<p>In ut sunt qui nostrud est consequat ut mollit ullamco aliquip dolor magna. Laborum magna ut eu incididunt nisi sit ullamco sint anim ex. Exercitation pariatur laborum non anim duis magna laboris velit officia ea adipisicing. Fugiat sint duis magna consectetur cupidatat ut voluptate anim et. Cillum laborum exercitation officia officia fugiat voluptate et laborum ut. Lorem ipsum dolor veniam culpa sint laboris reprehenderit cillum labore sunt sit ut in in ut aliquip excepteur id magna duis. Voluptate non ut minim reprehenderit aute et aliquip esse labore veniam laborum mollit.</p>
+	</>
+);
+
+export default AboutPage;
---
+++ my-blog-frontend/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = () => (
+	<>
+		<h1>Hello, welcome to my blog!</h1>
+
+		<p>In ut sunt qui nostrud est consequat ut mollit ullamco aliquip dolor magna. Laborum magna ut eu incididunt nisi sit ullamco sint anim ex. Exercitation pariatur laborum non anim duis magna laboris velit officia ea adipisicing. Fugiat sint duis magna consectetur cupidatat ut voluptate anim et. Cillum laborum exercitation officia officia fugiat voluptate et laborum ut. Lorem ipsum dolor veniam culpa sint laboris reprehenderit cillum labore sunt sit ut in in ut aliquip excepteur id magna duis. Voluptate non ut minim reprehenderit aute et aliquip esse labore veniam laborum mollit.</p>
+
+		<p>In ut sunt qui nostrud est consequat ut mollit ullamco aliquip dolor magna. Laborum magna ut eu incididunt nisi sit ullamco sint anim ex. Exercitation pariatur laborum non anim duis magna laboris velit officia ea adipisicing. Fugiat sint duis magna consectetur cupidatat ut voluptate anim et. Cillum laborum exercitation officia officia fugiat voluptate et laborum ut. Lorem ipsum dolor veniam culpa sint laboris reprehenderit cillum labore sunt sit ut in in ut aliquip excepteur id magna duis. Voluptate non ut minim reprehenderit aute et aliquip esse labore veniam laborum mollit.</p>
+
+		<p>In ut sunt qui nostrud est consequat ut mollit ullamco aliquip dolor magna. Laborum magna ut eu incididunt nisi sit ullamco sint anim ex. Exercitation pariatur laborum non anim duis magna laboris velit officia ea adipisicing. Fugiat sint duis magna consectetur cupidatat ut voluptate anim et. Cillum laborum exercitation officia officia fugiat voluptate et laborum ut. Lorem ipsum dolor veniam culpa sint laboris reprehenderit cillum labore sunt sit ut in in ut aliquip excepteur id magna duis. Voluptate non ut minim reprehenderit aute et aliquip esse labore veniam laborum mollit.</p>
+	</>
+);
+
+export default HomePage;
