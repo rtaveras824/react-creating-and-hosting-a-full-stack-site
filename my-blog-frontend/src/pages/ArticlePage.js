f636092687b2c67e6973170272383458e5892b63
Reuben Taveras <rtaveras824@hotmail.com>
2022-06-27 01:50:32 -0400

    Added comments to article page

--- my-blog-frontend/src/pages/ArticlePage.js
+++ my-blog-frontend/src/pages/ArticlePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import articleContent from './article-content';
 import ArticlesList from '../components/ArticlesList';
+import CommentsList from '../components/CommentsList';
 import NotFoundPage from './NotFoundPage';
 
 
@@ -31,6 +32,7 @@ const ArticlePage = () => {
 			{ article.content.map( (paragraph, key) => (
 				<p key={ key }>{ paragraph }</p>
 			))}
+			<CommentsList comments={ articleInfo.comments } />
 			<h3>Other Articles:</h3>
 			<ArticlesList articles={ otherArticles } />
 		</>
